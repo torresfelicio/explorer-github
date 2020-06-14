6265add1c27954b21cd56e806951f5f69cee60f3
torresfelicio <lucastorresfellicio@gmail.com>
2020-06-14 19:38:15 -0300

    adicionado api github

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,34 +1,63 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../styles/assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void>{
+    //adicionar um novo repositorio
+    event.preventDefault(); //prevenir o evento padrão do form
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
     <img src={logoImg} alt="Github Explorer" />
     <Title>Explore repositórios no Github</Title>
 
-    <Form>
-      <input type="text" placeholder="Digite o Repositorio aqui"/>
+    <Form onSubmit={handleAddRepository}>
+      <input
+      type="text"
+      placeholder="Digite o Repositorio aqui"
+      value={newRepo}
+      onChange={(e) => setNewRepo(e.target.value)}
+      />
       <button type="submit">Pesquisar</button>
     </Form>
 
     <Repositories>
-      <a href="">
+      {repositories.map(repository => (
+        <a key={repository.full_name} href="#">
         <img
-          src="https://avatars2.githubusercontent.com/u/18267941?s=460&u=935b3304de2d7ca3dd51e9618e4b68db707fd513&v=4"
-          alt="Imagem do repositorio"
+          src={repository.owner.avatar_url}
+          alt={repository.owner.login}
           />
           <div>
-            <strong>Lucas Torres</strong>
-            <p>Eu sou o lucas e gosto de tudo.</p>
+            <strong>{repository.full_name}</strong>
+            <p>{repository.description}</p>
           </div>
           <FiChevronRight size={20} />
       </a>
-
+      ))}
     </Repositories>
     </>
   )
