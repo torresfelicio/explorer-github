58727504a3d66be011a72d84bbda047bf1085171
torresfelicio <lucastorresfellicio@gmail.com>
2020-06-14 21:00:54 -0300

    modo dark

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -7,7 +7,7 @@ interface FormProps {
 
 export const Title = styled.h1`
   font-size: 48px;
-  color: #3A3A3A;
+  color: #3A3A4A;
   max-width: 450px;
   line-height: 56px;
 
@@ -26,7 +26,7 @@ export const Form = styled.form<FormProps>`
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
-    border: 2px solid #fff;
+    border: 3px solid #fff;
     border-right: 0px;
 
     ${(props) =>
